17db7ddab41280828bf98a3557c5887077965243
Nawabmeeravali <meeravalinawab7@gmail.com>
2020-05-17 14:03:34 +0530

    first commit

---
+++ reest.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 17 08:31:41 2020
+
+@author: quantum
+"""
+
+from flask import Flask ,jsonify
+reest = Flask(__name__)
+
+
+@reest.route('/')
+def hello():
+    return "Hello World!"
+
+@reest.route('/hello')
+def realhello():
+    return jsonify({"lemon":"Hello World!"}),201
+
+if __name__ == '__main__':
+    reest.run()
